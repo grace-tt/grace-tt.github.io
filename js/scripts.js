85316d867ca82b1d965d6978b3db4a2e4b6bcd05
grace-tt <60195389+grace-tt@users.noreply.github.com>
2024-03-10 21:37:44 -0400

    assign Pokemonlist and add objects
    add name height, types and egg_groups in the array and add to console

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
-alert('Hello world');
+/*assign pokemonlist*/
+let pokemonlist = [
+    {
+        Name: 'Charizard',
+        height: 1.7,
+        types: ['Fire', 'Flying'],
+        Egg_Groups: ['Monster', 'Dragon']
+        
+    },
+    {
+        Name: 'Lvysaur',
+        height: 1,
+        types: ['Grass', 'Poison'],
+        Egg_Groups: ['Monster', 'Dragon']
+        
+    },
+    {
+        Name: 'Spearow',
+        height: 0.3,
+        types: ['Fire', 'Normal'],
+        Egg_Groups: ['Flying']
+        
+    },
+    {
+        Name: 'Nidoking',
+        height: 1.4,
+        types: ['Ground', 'Poison'],
+        Egg_Groups: ['Monster', 'Field']
+        
+    }
+];
 
-
-/*assign variable named favoriteFood and assign it a value*/
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+console.log(pokemonlist);
