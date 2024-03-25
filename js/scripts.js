9bead11f06860709b9c9fd15dbc5aa0414ab1662
grace-tt <60195389+grace-tt@users.noreply.github.com>
2024-03-24 22:57:54 -0400

    update with foreach and getall add function
    replace the for loop with for each with IIFE function get all() and add() adding a new item in pokemonlist

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,66 @@
 /*assign pokemonlist*/
-let pokemonlist = [
+
+let pokemonRepository = (function () {
+  let pokemonlist = [
     {
-        Name: 'Charizard',
-        height: 1.7,
-        types: ['Fire', 'Flying'],
-        Egg_Groups: ['Monster', 'Dragon']
-        
+      Name: "Charizard",
+      height: 1.7,
+      types: ["Fire", "Flying"],
+      Egg_Groups: ["Monster", "Dragon"],
     },
     {
-        Name: 'Lvysaur',
-        height: 1,
-        types: ['Grass', 'Poison'],
-        Egg_Groups: ['Monster', 'Dragon']
-        
+      Name: "Lvysaur",
+      height: 1,
+      types: ["Grass", "Poison"],
+      Egg_Groups: ["Monster", "Dragon"],
     },
     {
-        Name: 'Spearow',
-        height: 0.3,
-        types: ['Fire', 'Normal'],
-        Egg_Groups: ['Flying']
-        
+      Name: "Spearow",
+      height: 0.3,
+      types: ["Fire", "Normal"],
+      Egg_Groups: ["Flying"],
     },
     {
-        Name: 'Nidoking',
-        height: 1.4,
-        types: ['Ground', 'Poison'],
-        Egg_Groups: ['Monster', 'Field']
-        
-    }
-];
+      Name: "Nidoking",
+      height: 1.4,
+      types: ["Ground", "Poison"],
+      Egg_Groups: ["Monster", "Field"],
+    },
+  ];
 
-
-/*console.log(pokemonlist);/
-
-/*add for loop to show every name in pokemonlist*/
-for (let i = 0; i < pokemonlist.length; i++) {
-/*if height is over 1.5, then showing 'wow, it is big!'*/
-  if (pokemonlist[i].height > 1.5) {
-    document.write("<br>"+ pokemonlist[i].Name + "\n" + "(height:" + pokemonlist[i].height +")" + " - Wow, it is big!");
-/*if height is less than 1.5, then only showing name and height*/
-  } else {
-    document.write("<br>"+pokemonlist[i].Name + "\n" + "(height:" + pokemonlist[i].height + ")" ); 
+  function add(pokemon) {
+    pokemonlist.push(pokemon);
   }
-}
 
+  function getAll() {
+    return pokemonlist;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+
+  /*for (let i = 0; i < pokemonlist.length; i++) { */
+})();
+
+pokemonRepository.add({ Name: "Pikachu", height: 1.04 });
+pokemonRepository.getAll().forEach(function (currentitem) {
+  if (currentitem.height > 1.5) {
+    document.write(
+      "<br>" +
+        currentitem.Name +
+        "\n" +
+        "(height:" +
+        currentitem.height +
+        ")" +
+        " - Wow, it is big!"
+    );
+  } else {
+    document.write(
+      "<br>" + currentitem.Name + "\n" + "(height:" + currentitem.height + ")"
+    );
+  }
+});
+
+console.log(pokemonRepository.getAll());
