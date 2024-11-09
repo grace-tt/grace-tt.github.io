9611a37be73277957a05f4b6419c108bf44a3898
grace-tt <60195389+grace-tt@users.noreply.github.com>
2024-11-09 18:36:05 -0500

    modification1
    using 5.3 version of bootstrap and modify Js code

--- js/scripts.js
+++ js/scripts.js
@@ -53,18 +53,10 @@ let pokemonRepository = (function () {
         pokemon.height = details.height;
         pokemon.types = details.types;
         pokemon.weight = details.weight;
-
-        console.log(pokemon);
-        showModal(pokemon);
-        return pokemon;
       })
       .catch(function (event) {
         console.error(event);
       });
-  }
-
-  function showDetails(pokemon) {
-    console.log("Hio,show deatails", pokemon);
   }
 
   function addEventListener(event, pokemon) {
@@ -79,7 +71,7 @@ let pokemonRepository = (function () {
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     listpokemon.classList = "list-group-item";
-    button.setAttribute("data-toggle", "modal");
+    button.setAttribute("data-toggle", "container");
     button.setAttribute("data-target", "#myModal");
     button.classList = "btn btn-secondary btn-lg btn-block";
 
@@ -89,6 +81,48 @@ let pokemonRepository = (function () {
     button.addEventListener("click", function (event) {
       showDetails(pokemon);
     });
+  }
+
+  function showDetails(pokemon) {
+    pokemonRepository.loadDetails(pokemon).then(function () {
+      showModal(pokemon);
+    });
+  }
+
+  function showModal(pokemon) {
+    let modalTitle = $(".modal-header");
+    let modalBody = $(".modal-body");
+    modalTitle.innerText = pokemon.name;
+
+    // Clear all existing modal content
+    // $("#myModal").on("shown.bs.modal", function () {
+    //   $("#myInput").trigger("focus");
+    // });
+    //creating lelment for name in modal content
+    let nameElement = $("<h1>" + pokemon.name + "</h1>");
+    //creating img in modal content
+    let imageElementFront = $('<img class="modal-img" style="width:50%">');
+    imageElementFront.attr("src", pokemon.imageUrlFront);
+    let imageElementBack = $('<img class="modal-img"style="width:50%">');
+    imageElementBack.attr("src", pokemon.imageUrlBack);
+    //creating element for height in modal content
+    let heightElement = $("<p>" + "height : " + pokemon.height + "</p>");
+    //creating element for weight in modal content
+    let weightElement = $("<p>" + "weight :" + pokemon.weight + "</p>");
+    //creating element for type in modal content
+    let typesElement = $("<p>" + "types : " + pokemon.types + "<p>");
+    //creating element for abilities in modal content
+    let abilitiesElement = $(
+      "<p>" + "abilties : " + pokemon.abilities + "<p/>"
+    );
+
+    modalTitle.append(nameElement);
+    modalBody.append(imageElementFront);
+    modalBody.append(imageElementBack);
+    modalBody.append(heightElement);
+    modalBody.append(weightElement);
+    modalBody.append(typesElement);
+    modalBody.append(abilitiesElement);
   }
 
   //open modal test//
@@ -116,37 +150,3 @@ pokemonRepository.LoadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-function showModal(pokemon) {
-  let modalTitle = $(".modal-header");
-  let modalBody = $(".modal-body");
-  modalTitle.innerText = pokemon.name;
-
-  // Clear all existing modal content
-  $("#myModal").on("shown.bs.modal", function () {
-    $("#myInput").trigger("focus");
-  });
-  //creating lelment for name in modal content
-  let nameElement = $("<h1>" + pokemon.name + "</h1>");
-  //creating img in modal content
-  let imageElementFront = $('<img class="modal-img" style="width:50%">');
-  imageElementFront.attr("src", pokemon.imageUrlFront);
-  let imageElementBack = $('<img class="modal-img"style="width:50%">');
-  imageElementBack.attr("src", pokemon.imageUrlBack);
-  //creating element for height in modal content
-  let heightElement = $("<p>" + "height : " + pokemon.height + "</p>");
-  //creating element for weight in modal content
-  let weightElement = $("<p>" + "weight :" + pokemon.weight + "</p>");
-  //creating element for type in modal content
-  let typesElement = $("<p>" + "types : " + pokemon.types + "<p>");
-  //creating element for abilities in modal content
-  let abilitiesElement = $("<p>" + "abilties : " + pokemon.abilities + "<p/>");
-
-  modalTitle.append(nameElement);
-  modalBody.append(imageElementFront);
-  modalBody.append(imageElementBack);
-  modalBody.append(heightElement);
-  modalBody.append(weightElement);
-  modalBody.append(typesElement);
-  modalBody.append(abilitiesElement);
-}
